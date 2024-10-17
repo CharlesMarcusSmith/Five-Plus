a6eec5ace2f72e0944f48155fe28e0ab1334feff
llamaladejam <charliemarcussmith@googlemail.com>
2024-10-17 21:39:33 +0100

    Basic navbar HTMl created. Toggle Menu icon functionality working.

--- src/components/Navbar/MainNavbar.jsx
+++ src/components/Navbar/MainNavbar.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { FiAlignJustify } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 
 export default function MainNavbar() {
+    // Create user icon click toggle functionality:
+    const[click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
   return (
-    <div><h1>Im a nav</h1></div>
+    <>
+    {/* this empty div is called fragmentation, and it's a way of avoiding unessesary divs in our project. */}
+    <header className='header'>
+        <div className="logo"><h1> CS </h1></div>
+        <div className="toggle" onClick={handleClick}>
+            {/* Ternary operator for conditional for icon toggle*/}
+            <span>{click ? <FiX/> : <FiAlignJustify/>}</span>
+        </div>
+        <ul classname="menu">
+            {/* 
+            multiple items created at once using following: 
+            li*4.menu_items 
+            */}
+            <li className="menu_items">Languages</li>
+            <li className="menu_items">Frameworks</li>
+            <li className="menu_items">Skills</li>
+            <li className="menu_items">Solutions</li>
+        </ul>
+    </header>
+    </>
   )
 }
