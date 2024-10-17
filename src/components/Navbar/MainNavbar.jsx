db38ce2ef71d295ba5df266dcfded35175a87de3
llamaladejam <charliemarcussmith@googlemail.com>
2024-10-17 23:42:17 +0100

    CSS styling for mobile nav complete.

--- src/components/Navbar/MainNavbar.jsx
+++ src/components/Navbar/MainNavbar.jsx
@@ -1,31 +1,31 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 import { FiAlignJustify } from "react-icons/fi";
 import { FiX } from "react-icons/fi";
-
+import "./MainNavbar.css";
 export default function MainNavbar() {
     // Create user icon click toggle functionality:
     const[click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
   return (
     <>
-    {/* this empty div is called fragmentation, and it's a way of avoiding unessesary divs in our project. */}
-    <header className='header'>
-        <div className="logo"><h1> CS </h1></div>
-        <div className="toggle" onClick={handleClick}>
-            {/* Ternary operator for conditional for icon toggle*/}
-            <span>{click ? <FiX/> : <FiAlignJustify/>}</span>
-        </div>
-        <ul classname="menu">
-            {/* 
-            multiple items created at once using following: 
-            li*4.menu_items 
-            */}
-            <li className="menu_items">Languages</li>
-            <li className="menu_items">Frameworks</li>
-            <li className="menu_items">Skills</li>
-            <li className="menu_items">Solutions</li>
-        </ul>
-    </header>
+      {/* this empty div is called fragmentation, and it's a way of avoiding unessesary divs in our project. */}
+      <header className='header'>
+          <div className="logo"><h1> CS </h1></div>
+          <div className="toggle" onClick={handleClick}>
+              {/* Ternary operator for conditional for icon toggle*/}
+              <span>{click ? <FiX/> : <FiAlignJustify/>}</span>
+          </div>
+          <ul className={click ? "menu active" : "menu"}>
+              {/* 
+              multiple items created at once using following: 
+              li*4.menu_items 
+              */}
+              <li className="menu_items">Languages</li>
+              <li className="menu_items">Frameworks</li>
+              <li className="menu_items">Skills</li>
+              <li className="menu_items">Solutions</li>
+          </ul>
+      </header>
     </>
   )
 }
