5812fbc955b86f1a28ddef9d707bfe1d204f6b0a
llamaladejam <charliemarcussmith@googlemail.com>
2024-10-19 14:33:29 +0100

    Language page mobile hard coded.

--- src/components/Navbar/LanguagesBody.jsx
+++ src/components/Navbar/LanguagesBody.jsx
@@ -1,27 +1,37 @@
 import "./LanguagesBody.css"
+import { TbStar } from "react-icons/tb";
+import { TbStarFilled } from "react-icons/tb";
 export default function LanguagesBody() {
     return (
         <>
             <div className="container">
                 <ul className="lang_list">
-                    <h2>Languages</h2>
+                    <h2 className="lang_heading">Languages</h2>
                     <li className="lang_items">Java</li>
                     <li className="lang_items">Kotlin</li>
                     <li className="lang_items">Python</li>
                     <li className="lang_items">SQL</li>
                 </ul>
                 <div className="lang_body">
-                    <p className="lang_text">
+                    <span className="lang_stars">
+                        <TbStarFilled/><TbStarFilled/><TbStar/><TbStar/><TbStar/>
+                    </span>
+                    <div className="lang_text">
+                        <h2>SQL</h2>
+                        <h4 className="stars_wording">Advanced Beginner</h4>
+                        <p className="text_heading">Covered</p>
                         <p>Have completed a number of projects involving SQL, covering:</p>
-                        <p>SELECT</p>
-                        <p>INSERT</p>
-                        <p>CREATE TABLE</p>
-                        <p>JOIN</p>
-                        <p>CREATE EXTERNAL TABLE</p>
-                        <p>SHOW TABLES</p>
-                        <p>DROP IF EXISTS</p>
-                        <p>LOAD DATA LOCAL INPATH</p>
-                    </p>
+                        <ul>
+                            <li className="covered_bullet"><span>SELECT</span></li>
+                            <li className="covered_bullet"><span>INSERT</span></li>
+                            <li className="covered_bullet"><span>CREATE TABLE</span></li>
+                            <li className="covered_bullet"><span>JOIN</span></li>
+                            <li className="covered_bullet"><span>CREATE EXTERNAL TABLE</span></li>
+                            <li className="covered_bullet"><span>SHOW TABLES</span></li>
+                            <li className="covered_bullet"><span>DROP IF EXISTS</span></li>
+                            <li className="covered_bullet"><span>LOAD DATA LOCAL INPATH</span></li>
+                        </ul>
+                    </div>
                 </div>
             </div>        
         </>
